b7e99aa1c46de674089b3969f80e8bc29b99a266
paulmarshall <paul@nevada.unr.edu>
2018-09-23 19:59:46 -0700

    Uh, this is disgusting

--- project1.py
+++ project1.py
@@ -18,7 +18,7 @@ def main():
 	X = np.array([[1,0],[7,4],[9,6],[2,1],[4,8],[0,3],[13,5],[6,8],[7,3],[3,6],[2,1],[8,3],[10,2],[3,5],[5,1],[1,9],[10,3],[4,1],[6,6],[2,2]])
 	X = np.array([[0],[1],[2],[7],[8],[9],[12],[14],[15]])
 	K = 3
-	K_Means(X,K)
+	#K_Means(X,K)
 
 	# Training Set 1:
 	X_train_1 = np.array([[0,1], [0,0], [1,0], [0,0], [1,1]])
@@ -41,13 +41,31 @@ def main():
 
 	max_depth = 4
 
-	#DT_train_binary(X_train_2,Y_train_2,max_depth)
+	Trained_DT = DT_train_binary(X_train_1,Y_train_1,max_depth)
+	Accuracy = DT_test_binary(X_test_1,Y_test_1,Trained_DT)
+
+class Tree(object):
+    def __init__(self):
+        self.left = None
+        self.right = None
+        self.left_features_used = []
+        self.right_features_used = []
+        self.left_points = []
+        self.right_points = []
+        self.left_accuracy = 0
+        self.right_accuracy = 0
+        self.feature_index = None
+        self.accuracy = None
 
 # DT section
 def DT_train_binary(X,Y,max_depth):
 	
 	num_features = len(X[0])
 	accuracy_array = []
+	DT_array = []
+	feature_used_array = []
+	DT = Tree()
+	highest_accuracy = 0
 
 	for index in range(num_features):
 		accuracy_array.append(0)
@@ -64,18 +82,186 @@ def DT_train_binary(X,Y,max_depth):
 		accuracy_array[feature_counter] = accuracy_array[feature_counter] / sample_counter
 		feature_counter += 1
 
+	#Build DT Root
+	for result in accuracy_array:
 
-	#Build DT
-	if max_depth == -1:
-		DT = np.zeros((max_depth,2))
+		if result == 1.0:
+			DT.feature_index = accuracy_array.index(result)
+			DT.left = 0
+			DT.right = 1
+			DT.accuracy = result
+			return DT
 
-	DT = np.zeros((max_depth,2))
+		elif result == 0.0:
+			DT.feature_index = accuracy_array.index(result)
+			DT.left = 1
+			DT.right = 0
+			DT.accuracy = result
+			return DT
+
+		else:
+			if result > highest_accuracy:
+				highest_accuracy = result
+
+	DT.feature_index = accuracy_array.index(highest_accuracy)
+	DT.left = 0
+	DT.right = 1
+	DT.accuracy = highest_accuracy
+	sample_counter = 0
+	accuracy_counter_left = 0
+	accuracy_counter_right = 0
+
+	for sample in X:
+		if sample[DT.feature_index] == Y[sample_counter]:
+			if Y[sample_counter] == DT.left:
+				accuracy_counter_left += 1
+			else:
+				accuracy_counter_right += 1
+		if sample[DT.feature_index] == DT.left:
+			sample = list(sample)
+			sample.append(sample_counter)
+			sample = np.array(sample)
+			DT.left_points.append(sample)
+		else:
+			sample = list(sample)
+			sample.append(sample_counter)
+			sample = np.array(sample)
+			DT.right_points.append(sample)
+
+		sample_counter += 1
+
+	DT.left_accuracy = accuracy_counter_left / sample_counter
+	DT.right_accuracy = accuracy_counter_right / sample_counter
+	DT = DT_Recursion_Helper(DT,X,Y,max_depth)
 
 
-	print(accuracy_array)
+
+	return DT
+
+def DT_Recursion_Helper(DT,X,Y,max_depth):
+	
+	accuracy_array = []
+	num_features = len(X[0])
+	feature_counter = 0
+
+	for index in range(num_features):
+		accuracy_array.append(0)
+
+	if (DT.accuracy == 1.0):
+		return DT
+
+	#Build left tree, build right tree
+	print(np.array(DT.left_points).T)
+	sample_array = np.array(DT.left_points).T[-1:]
+	left_points_transpose = np.array(DT.left_points).T[:-1]
+
+	for feature_col in left_points_transpose: #For each feature, calculate accuracy
+		sample_counter = 0
+		for sample_feature in feature_col:
+			if sample_feature == Y[sample_array[0][sample_counter]]:
+				accuracy_array[feature_counter] = accuracy_array[feature_counter]+1
+			sample_counter += 1
+
+		accuracy_array[feature_counter] = accuracy_array[feature_counter] / sample_counter
+		feature_counter += 1
+
+	highest_accuracy = DT.left_accuracy
+
+	for result in accuracy_array:
+		if result == 1.0:
+			DT.left = Tree()
+			DT.left.feature_index = accuracy_array.index(result)
+			DT.left.left = 0
+			DT.left.right = 1
+			DT.left.accuracy = result
+			return DT
+
+		elif result == 0.0:
+			DT.left = Tree()
+			DT.left.feature_index = accuracy_array.index(result)
+			DT.left.left = 1
+			DT.left.right = 0
+			DT.left.accuracy = result
+			return DT
+		else:
+			if result > highest_accuracy:
+				highest_accuracy = result
+
+		if highest_accuracy != DT.left_accuracy:
+			print(highest_accuracy)
+			DT.left = Tree()
+			DT.left.feature_index = accuracy_array.index(highest_accuracy)
+			DT.left.left = 0
+			DT.left.right = 1
+			DT.left.accuracy = highest_accuracy
+			sample_counter = 0
+			accuracy_counter_left = 0
+			accuracy_counter_right = 0
+
+			for sample in X:
+				if sample[DT.feature_index] == Y[sample_counter]:
+					if Y[sample_counter] == DT.left:
+						accuracy_counter_left += 1
+					else:
+						accuracy_counter_right += 1
+				if sample[DT.feature_index] == DT.left:
+					DT.left_points.append(sample)
+				else:
+					DT.right_points.append(sample)
+
+				sample_counter += 1
+
+	DT.accuracy = DT.left_accuracy + DT.right_accuracy
+	return DT
+
+def Print_DT(DT):
+	print("Feature:", DT.feature_index, "Left:")
+	if not type(DT.left) == int:
+		print("  ")
+		Print_DT(DT.left)
+	else:
+		print(DT.left)
+	print("Right:")
+
+	if not type(DT.right) == int:
+		print("  ")
+		Print_DT(DT.right)
+	else:
+		print(DT.right)
 
 
+def DT_test_binary(X,Y,DT):
+	Print_DT(DT)
 
+	accuracy_counter = 0
+	sample_counter = 0
+
+	for sample in X:
+		
+		if Y[sample_counter] == DT_test_binary_helper(sample,DT):
+			accuracy_counter += 1
+		sample_counter += 1
+
+	accuracy = accuracy_counter/sample_counter
+	print("Accuracy: ", accuracy)
+	return accuracy
+
+
+def DT_test_binary_helper(sample,DT):
+	accuracy_counter = 0
+	print(DT.left, DT.right)
+	if sample[DT.feature_index] == 0:
+		if type(DT.left) == int:
+			print("Return: ", DT.left)
+			return DT.left
+		else:
+			return DT_test_binary_helper(sample,DT.left)
+	else:
+		if type(DT.right) == int:
+			print("Return: ", DT.right)
+			return DT.right
+		else:
+			return DT_test_binary_helper(sample,DT.right)
 
 #DT_test_binary(X,Y,DT):
 
